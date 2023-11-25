f660f74a8b06cf20f5492d7abdc7768f686325c8
DavorViz <dvizintin@veleri.hr>
2023-11-25 11:07:17 +0100

    Promjene subota - readme i readme2

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Dodajemo neki novi text. I opet - promjena 1!
 =======
 Dodajemo neki novi text. I opet! I još jednom!! - promjena 2!
 >>>>>>> c4c7b7374e51c5cc870df29777665ff438b3aa6a
+
+Još malo promjena - subota 25.11.
--- php/sve_ponude.php
+++ php/sve_ponude.php
@@ -22,6 +22,20 @@
     <div class="w3-container">
     <h1>Web shop - mobiteli</h1>    
     <p></p>
+
+    <?php
+
+    $server = "localhost";
+    $database = "mobiteli";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+    $query = "SELECT * FROM mobiteli";
+    $res = mysqli_query($conn, $query);
+
+    ?>
+
     <div>Popis svih mobitela:
         <table class="w3-table-all">
             <tr class="w3-red">
@@ -32,6 +46,19 @@
                 <th>Slika</th>
                 <th>Cijena</th>
             </tr>
+            <?php
+            while($row = mysqli_fetch_array($res)){
+                echo "<tr>";
+                echo "<td>".$row['ID']."</td>";
+                echo "<td>".$row['NAZIV']."</td>";
+                echo "<td>".$row['PROIZVODJAC']."</td>";
+                echo "<td>".$row['OPIS']."</td>";
+                echo "<td><img src='".$row['SLIKA']."'width='100px' alt='".$row['NAZIV']."'</td>";
+                echo "<td>".$row['CIJENA']."</td>";
+                echo "</tr>";
+            }
+            mysqli_close($conn);
+            ?>
             <tr>
                 <td>1.</td>
                 <td>Galaxy Z Flip5</td>
